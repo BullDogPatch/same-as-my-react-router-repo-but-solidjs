9000483efa3088b5f69d1c2bfbd76bd4515f4b8e
Craig Clayton <craig__clayton@outlook.com>
2024-02-29 18:57:16 +0000

    install axios

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Router } from "@solidjs/router";
+import { Router, Route } from "@solidjs/router";
 
 import "./index.css";
 import App from "./App";
+import ProfilePage from "./components/ProfilePage";
+import Home from "./components/Home";
 
 const root = document.getElementById("root");
 
 render(
   () => (
-    <Router>
-      <App />
+    // <Router>
+    //   <App />
+    // </Router>
+    <Router root={App}>
+      <Route path="/profiles/:username" component={ProfilePage} />
+      <Route path="/" component={Home} />
     </Router>
   ),
   root
